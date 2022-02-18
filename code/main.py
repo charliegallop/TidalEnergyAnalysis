7327cc21924e35c221f37d2ea549b9382461dba6
charliegallop <c.gallop@protonmail.com>
2022-02-18 20:05:27 +0000

    Added moonphase icon to the images

--- code/main.py
+++ code/main.py
@@ -3,6 +3,8 @@
 from functions import CreateImageStack
 from functions import CreateImageStackStreamplot
 import netCDF4 as nc
+from PIL import Image
+import pandas as pd
 
 fn = "/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/data/rawData/TIGER_Model_2019-11/flow/output/TIGER_map.nc"
 
@@ -39,9 +41,42 @@ places = {
     'Le Conquet': [(48.359961, -4.774587), (150, 0)]
     }
 
+
+#Moon Phases
+    # 0 – new moon
+    # 0-0.25 – waxing crescent
+    # 0.25 – first quarter
+    # 0.25-0.5 – waxing gibbous
+    # 0.5 – full moon
+    # 0.5-0.75 – waning gibbous
+    # 0.75 – last quarter
+    # 0.75 -1 – waning crescent
+
+new = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/new.png')
+full = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/full.png')
+waningGibbous = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/waningGibbous.png')
+waxingGibbous = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/waxingGibbous.png')
+waningCrescent = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/waningCrescent.png')
+waxingCrescent = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/waxingCrescent.png')
+firsrtQuarter = Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/firstQuarter.png')
+thirdQuarter =  Image.open(r'/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/images/thirdQuarter.png')
+
+moonPhasesImgs = {
+    "new": new,
+    "full": full,
+    "waningGibbous": waningGibbous,
+    "waxingGibbous": waxingGibbous,
+    "waningCrescent": waningCrescent,
+    "waxingCrescent": waxingCrescent,
+    "firstQuarter": firsrtQuarter,
+    "thirdQuarter": thirdQuarter
+    }
+
+moonPhases = pd.read_csv("/home/charlie/Documents/Uni/Exeter - Data Science/MTHM604_Tackling_Sustainability_Challenges/MTHM604_week_2/data/rawData/Dover 2019-10-26 to 2019-12-31.csv")
+moonPhases['datetime'] = pd.to_datetime(moonPhases['datetime']).dt.date
 # Ideal Parameters for Channel map:
     # groupAmount = 3
     # numArrows = 10
 
-CreateImageStack(clippedChannelArea, fn, timeRange = [6, 500], mapRes = "i", groupAmount=3, numArrows=10, places = places)
+CreateImageStack(clippedChannelArea, fn, timeRange = [630, 660], mapRes = "i", groupAmount=3, numArrows=9, places = places, moonImgs = moonPhasesImgs, moonPhases = moonPhases)
 #CreateImageStackStreamplot(falArea, fn, timeRange=[100, 105], mapRes = "f", groupAmount = 4)
